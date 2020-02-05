554ebac577b398bdac1052baf014b3ff5e13f7e4
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-02-05 17:17:47 +0000

    Create utilities.R

---
+++ R/utilities.R
@@ -0,0 +1,22 @@
+#############################################################################
+################################ Utilities ##################################
+#############################################################################
+
+# hidden
+nullToNA <- function(x) {
+  x[sapply(x, is.null)] <- NA
+  return(x)
+}
+
+# hidden
+break_into_subtrees <- function(x){
+  nlist = nat::neuronlist()
+  for(i in 1:x$nTrees){
+    segs = x$SubTrees[[i]]
+    d = x$d[unique(unlist(segs)),]
+    rownames(d) = 1:nrow(d)
+    n = nat::as.neuron(d)
+    nlist  = c(nlist, nat::as.neuronlist(n))
+  }
+  nlist
+}
