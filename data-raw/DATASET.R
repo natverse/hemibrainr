af7998da667c1910567066b2d3ce25f4b06788cf
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-02-20 13:57:44 +0000

    Added precomputed metrics for all neurons, broken down by axon and dendrite
    * Added data.frame, hemibrain_metrics

---
+++ data-raw/DATASET.R
@@ -0,0 +1,6 @@
+## code to prepare `DATASET` dataset goes here
+hemibrain_splitpoints_distance <- read.csv("data-raw/hemibrain_all_neurons_splitpoints.csv")
+hemibrain_metrics <- read.csv("data-raw/hemibrain_all_neurons_metrics.csv")
+rownames(hemibrain_metrics) <- hemibrain_metrics$bodyid
+usethis::use_data(hemibrain_splitpoints_distance, overwrite = TRUE)
+usethis::use_data(hemibrain_metrics, overwrite = TRUE)
