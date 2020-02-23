ea3a0ad1f51e59fd7bf3f0ba38f61054e37f8859
Gregory Jefferis <jefferis@gmail.com>
2020-02-23 08:06:31 +0000

    Add pre/post thresholds to grouped_adjacency
    * pre threshold to avoid NAs after scaling

--- R/adjacency.R
+++ R/adjacency.R
@@ -1,9 +1,14 @@
 #' Flexible grouping of rows/columns of a raw adjacency matrix
 #'
-#' Use this to create a summary adjacency matrix in which input and/or output
-#' neurons are coalesced into a smaller number of rows/columns. This can be used
-#' as a preprocessing step to present a simple representation of the
-#' connectivity between a larger number of cells in a heatmap.
+#' Create a summary adjacency matrix in which input and/or output neurons are
+#' coalesced into a smaller number of rows/columns. This can be used as a
+#' preprocessing step to present a simple representation of the connectivity
+#' between a larger number of cells in a heatmap.
+#'
+#' @details The default value of threshold is \code{c(1, 0)}. When
+#'   \code{scale=F} this will not apply a treshold. When \code{scale=T} this
+#'   will apply a threshold of 1 to the raw counts before scaling to ensure that
+#'   only valid output will be returned.
 #'
 #' @param inputids Either the bodyids of the input neurons OR an adjacency
 #'   matrix
@@ -11,12 +16,16 @@
 #'   \code{inputids} is an adjacency matrix.
 #' @param ingroup,outgroup grouping variables used to coalesce related neurons
 #' @param threshold Remove (groups of) neurons that make fewer connections than
-#'   this.
-#' @param scale Whether to scale the rows or columns so that they sum to 1
+#'   this. If you provide a length two vector the first threshold will be an
+#'   absolute value applied before scaling, while the second will be a
+#'   fractional value applied after scaling.
+#' @param scale Whether to scale the rows or columns so that they sum to 1.
+#'   Default is \code{'none'}.
 #' @param ... Additional arguments passed to
 #'   \code{\link{neuprint_get_adjacency_matrix}}
 #'
-#' @return named numeric matrix
+#' @return named numeric matrix with rows for input groups and columns for
+#'   output groups.
 #' @seealso \code{\link{neuprint_get_adjacency_matrix}}
 #' @export
 #'
@@ -24,13 +33,34 @@
 #' \donttest{
 #' pnkc=grouped_adjacency(class2ids("PN"), 'KC')
 #' heatmap(pnkc)
+#'
+#' # alternatively, if you want to play around with different arguments,
+#' # you can get the raw adjacency matrix and then group that in different
+#' # ways
+#' pnkc.raw=neuprint_get_adjacency_matrix(inputids = class2ids("PN"),
+#'   outputids = 'KC')
+#' pnkc.bytype=grouped_adjacency(pnkc.raw, ingroup = "type", outgroup = "type")
+#' heatmap(pnkc.bytype)
+#'
+#' # slightly finer groups
+#' heatmap(grouped_adjacency(pnkc.raw, ingroup = "type", outgroup = "name"))
+#' # add a threshold to remove neurons that make very few connections
+#' heatmap(grouped_adjacency(pnkc.raw, ingroup = "type", outgroup = "name",
+#'   threshold=50))
+#'
+#' # Trying scaling to see relative input onto different groups
+#' # of Kenyon cell target neurons. col=>
+#' heatmap(grouped_adjacency(pnkc.raw, ingroup = "type", outgroup = "name",
+#'   scale='col', threshold = c(1,0.01)))
 #' }
 #' @importFrom neuprintr neuprint_get_adjacency_matrix neuprint_get_meta
 grouped_adjacency <- function(inputids=NULL, outputids=NULL,
                               ingroup=c("type", "name", "cellBodyFiber"),
                               outgroup=c("type", "name", "cellBodyFiber"),
-                              threshold=0,
+                              threshold=c(1, 0),
                               scale=c("none", "col", "row"), ...) {
+  if(length(threshold)>2)
+    stop("Only two threshold levels allowed!")
 
   if(is.matrix(inputids)){
     am <- inputids
@@ -95,6 +125,13 @@ grouped_adjacency <- function(inputids=NULL, outputids=NULL,
     am=sapply(l, colSums)
   }
 
+  if(length(threshold)>1) {
+    if(isFALSE(scale=='none')) {
+      if(threshold[1]>0) am=am[, !colSums(am)<threshold[1], drop=F]
+      if(threshold[1]>0) am=am[!rowSums(am)<threshold[1], , drop=F]
+    }
+    threshold=threshold[2]
+  }
   scale=match.arg(scale)
   if(scale=="row") {
     am <- t(scale(t(am), center = FALSE, scale=rowSums(am)))
