d2d7721e93695ccb658580301cfee352fec5ec34
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-03-05 22:30:17 +0000

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -36,3 +36,13 @@ is.hxsurf <- function(x){
 #' @importFrom nat progress_natprogress
 #' @export
 nat::progress_natprogress
+
+# Get the Nth highest number
+maxN <- function(x, N=2){
+  len <- length(x)
+  if(N>len){
+    warning('N greater than length(x).  Setting N=length(x)')
+    N <- length(x)
+  }
+  sort(x,partial=len-N+1)[len-N+1]
+}
