e6e4e00b2c70e9f1178537343b90f72a7d86aefa
Gregory Jefferis <jefferis@gmail.com>
2020-04-05 11:44:11 +0100

    Improve docs / examples for hemibrain_roi_meshes

--- R/hemibrain_meshes.R
+++ R/hemibrain_meshes.R
@@ -2,19 +2,22 @@
 ################################ meshes ##################################
 ##########################################################################
 
-#' Get all the region of interest meshes in the hemibrain project
+#' Get some or all the region of interest meshes in the hemibrain project
 #'
-#' @description Get all the ROI meshes as named \code{rgl::mesh3d} objects.
+#' @description This function can retrieve some or all the hemibrain project ROI
+#'   meshes as \code{rgl::\link{mesh3d}} objects. They are returned in a named
+#'   \code{\link{neuronlist}} object. This allows further subsets and colours to
+#'   be applied via the standard \code{nat::\link{plot3d.neuronlist}} semantics.
+#'   See the examples for further details.
 #'
 #' @inheritParams hemibrain_read_neurons
 #' @inheritParams neuprintr::neuprint_ROIs
 #' @inheritParams nat::nlapply
 #' @param rois The ROIs to fetch (default \code{NULL} implies all ROIs)
 #' @param ... Additional arguments passed to \code{\link{nlapply}}
-#' @return a list of \code{rgl::mesh3d} objects
+#' @return a \code{neuronlist} of \code{rgl::mesh3d} objects
 #'
 #' @examples
-#' \donttest{
 #' \dontrun{
 #' # Get all the roi meshes
 #' hemibrain.rois = hemibrain_roi_meshes()
@@ -25,9 +28,21 @@
 #'     plot3d(hemibrain.rois[[m]],alpha = 0.5, col = cols[m])
 #' }
 #'
-#' }}
+#' # Alternatively plot a subset all at once with rainbow colours
+#' # making use of nat::plot3d.neuronlist
+#' plot3d(hemibrain.rois, 1:3, add=TRUE)
+#'
+#' # If you have neuprintr >= 1.0 you can easily find groups of meshes
+#' # find just the terminal nodes of the MB ROI hierarchy
+#' library(nat)
+#' mbterms=nat::endpoints(neuprint_ROI_hierarchy(root='MB(R)', rval='graph'))
+#' mb.rois=hemibrain_roi_meshes(mbterms)
+#' plot3d(mb.rois, add=TRUE)
+#' }
 #' @export
-#' @seealso \code{\link{hemibrain_skeleton_check}}, \code{\link{hemibrain.surf}}
+#' @seealso \code{\link{hemibrain_skeleton_check}}, \code{\link{hemibrain.surf}},
+#' \code{neuprintr::\link[neuprintr]{neuprint_ROI_mesh}},
+#' \code{neuprintr::\link[neuprintr]{neuprint_ROI_hierarchy}}
 #' @importFrom neuprintr neuprint_ROIs neuprint_ROI_mesh
 hemibrain_roi_meshes <- function(rois=NULL, microns = FALSE, superLevel=NULL, OmitFailures=TRUE, conn=NULL, dataset=NULL, ...){
   if(isTRUE(is.null(rois)))
