540a0ed60bd28e65dff7ecf6bcb3b6988247f692
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-04-07 21:50:09 +0100

    Update hemibrain_split_all_neurons.R

--- data-raw/hemibrain_split_all_neurons.R
+++ data-raw/hemibrain_split_all_neurons.R
@@ -3,7 +3,7 @@
 
 # Parameters
 polypre = TRUE
-split = "synapses"
+split = "distance"
 mode = "centrifugal"
 identifier = paste(ifelse(polypre,"polypre","pre"),mode,split,sep="_")
 message("Splitting with paramaters: ", identifier)
