8e8330e5e6f964cc3a36e2fcd46f0f072576268d
Gregory Jefferis <jefferis@gmail.com>
2020-04-18 17:17:10 +0100

    hemibrain_read_neurons warns when *no* neurons are missing!
    * i.e. the warning logic is backwards
    * this is only when using a local savedir

--- R/hemibrain_read_neurons.R
+++ R/hemibrain_read_neurons.R
@@ -64,7 +64,7 @@ hemibrain_read_neurons<-function(x = NULL,
       if(!is.null(neurons.flow.fh)){
         y = intersect(x,names(neurons.flow.fh))
         z = setdiff(x,names(neurons.flow.fh))
-        if(!length(z)){
+        if(length(z)>0){
           warning("The following bodyids could not be read from your saved neurolistfh object. If they exist, you
                 may need to read them from neuPrint by changing the savedir argument of this function to FALSE: ",
                   paste(z,collapse=", "))
