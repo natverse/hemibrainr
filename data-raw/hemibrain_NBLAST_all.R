508dc44223caf3db5ca7fd186ea3cbe04167277a
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-04-23 17:39:05 +0100

    Get a aba nblast for neurons simplifie to n = 1

--- data-raw/hemibrain_NBLAST_all.R
+++ data-raw/hemibrain_NBLAST_all.R
@@ -28,20 +28,26 @@ message("Using ", numCores/2, " cores")
 load(paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_flow_",identifier,".rda"))
 message("Loaded: ", length(all.neurons.flow), " neurons")
 
+# Scale
+all.neurons.flow.microns.microns = hemibrainr:::scale_neurons.neuronlist(all.neurons.flow, .parallel = TRUE)
+message("Neurons scaled!")
+
 # Extract compartments
-all.neurons.pnt = primary_neurite_cable(x = all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
-all.neurons.pd = primary_dendrite_cable(x = all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
-all.neurons.axon = axonic_cable(x = all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
-all.neurons.dendrite = dendritic_cable(x = all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
-all.neurons.arbour = arbour_cable(x = all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.pnt = primary_neurite_cable(x = all.neurons.flow.microns, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.pd = primary_dendrite_cable(x = all.neurons.flow.microns, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.axon = axonic_cable(x = all.neurons.flow.microns, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.dendrite = dendritic_cable(x = all.neurons.flow.microns, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.arbour = arbour_cable(x = all.neurons.flow.microns, .parallel = TRUE, OmitFailures = TRUE)
+all.neurons.spine = nat::nlapply(X = all.neurons.flow.microns, FUN = nat::simplify_neuron, n = 1, invert = FALSE, .parallel = TRUE, OmitFailures = TRUE)
 
 # Make dps objects
-## hemibrain.twigs5.dps=dotprops(all.neurons.flow/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
-hemibrain.dps.pnt=dotprops(all.neurons.pnt/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
-hemibrain.dps.pd=dotprops(all.neurons.pd/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
-hemibrain.dps.axon=dotprops(all.neurons.axon/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
-hemibrain.dps.dendrite=dotprops(all.neurons.dendrite/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
-hemibrain.dps.arbour=dotprops(all.neurons.arbour/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
+## hemibrain.twigs5.dps=dotprops(all.neurons.flow.microns/1e3, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.pnt=dotprops(all.neurons.pnt, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.pd=dotprops(all.neurons.pd, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.axon=dotprops(all.neurons.axon, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.dendrite=dotprops(all.neurons.dendrite, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.arbour=dotprops(all.neurons.arbour, k=5, resample=1, .parallel=T, OmitFailures = T)
+hemibrain.dps.spine=dotprops(all.neurons.spine, k=5, resample=1, .parallel=T, OmitFailures = T)
 
 # Forward FIB->FIB nblasts (normalised)
 # hemibrain.twigs5.aba.mean=nat.nblast::nblast_allbyall(hemibrain.twigs5.dps,
@@ -91,4 +97,10 @@ hemibrain.arbour.aba.mean=nat.nblast::nblast_allbyall(hemibrain.dps.arbour,
 hemibrainr:::save_compressed_nblast_mat(hemibrain.arbour.aba.mean,
                                         file = nblastfolder)
 
-
+### NBLAST spine
+hemibrain.spine.aba.mean=nat.nblast::nblast_allbyall(hemibrain.dps.spine,
+                                                      .progress='text',
+                                                      .parallel=TRUE,
+                                                      normalisation='mean')
+hemibrainr:::save_compressed_nblast_mat(hemibrain.spine.aba.mean,
+                                        file = nblastfolder)
