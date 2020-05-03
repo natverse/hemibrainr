6f9b9e80988c9caf666be4f82d7ae0315a7f3068
Gregory Jefferis <jefferis@gmail.com>
2020-05-03 09:06:00 +0100

    formatting

--- R/hemibrain_colours.R
+++ R/hemibrain_colours.R
@@ -24,25 +24,51 @@
 #' par(mar = rep(0, 4))
 #' pie(rep(1, length(hemibrain_bright_colours)), col = hemibrain_bright_colours)
 #'
-#'}
+#' }
 #' @name hemibrain_colours
 #' @export
 #' @importFrom grDevices colorRampPalette
-hemibrain_colours <- hemibrain_colors <- c(red = "#A53600", magenta = "#B32DB5", midblue = "#0072B2",
-                                           darkgold = "#908827", green = "#348E53", blue = "#053CFF")
+hemibrain_colours <-
+  hemibrain_colors <-
+  c(
+    red = "#A53600",
+    magenta = "#B32DB5",
+    midblue = "#0072B2",
+    darkgold = "#908827",
+    green = "#348E53",
+    blue = "#053CFF"
+  )
 #' @rdname hemibrain_colours
 #' @export
 hemibrain_colour_ramp = grDevices::colorRampPalette(hemibrain_colours)
 
 #' @rdname hemibrain_colours
 #' @export
-hemibrain_bright_colours <- c(purple = "#C70E7B", pink = "#FC6882", blue = "#007BC3", cyan = "#54BCD1",
-                                                         darkorange = "#EF7C12", paleorange = "#F4B95A", darkgreen = "#009F3F",
-                                                         green = "#8FDA04", brown = "#AF6125", palebrown = "#F4E3C7",
-                                                         mauve = "#B25D91", lightpink = "#EFC7E6", orange = "#EF7C12",
-                                                         midorange = "#F4B95A", darkred = "#C23A4B", darkyellow = "#FBBB48",
-                                                         yellow = "#EFEF46", palegreen = "#31D64D", navy = "#132157",
-                                                         cerise = "#EE4244", red = "#D72000", marine = "#1BB6AF")
+hemibrain_bright_colours <-
+  c(
+    purple = "#C70E7B",
+    pink = "#FC6882",
+    blue = "#007BC3",
+    cyan = "#54BCD1",
+    darkorange = "#EF7C12",
+    paleorange = "#F4B95A",
+    darkgreen = "#009F3F",
+    green = "#8FDA04",
+    brown = "#AF6125",
+    palebrown = "#F4E3C7",
+    mauve = "#B25D91",
+    lightpink = "#EFC7E6",
+    orange = "#EF7C12",
+    midorange = "#F4B95A",
+    darkred = "#C23A4B",
+    darkyellow = "#FBBB48",
+    yellow = "#EFEF46",
+    palegreen = "#31D64D",
+    navy = "#132157",
+    cerise = "#EE4244",
+    red = "#D72000",
+    marine = "#1BB6AF"
+  )
 
 #' @rdname hemibrain_colours
 #' @export
