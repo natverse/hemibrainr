d5c0abf6fff182f79f6cd933759f68dba1f0298c
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-09-19 11:55:57 +0100

    Update hemibrain_split_all_neurons.R

--- data-raw/hemibrain_split_all_neurons.R
+++ data-raw/hemibrain_split_all_neurons.R
@@ -31,7 +31,8 @@ all.neurons.flow = flow_centrality(all.neurons.checked, mode = mode, polypre = p
 missed = setdiff(names(all.neurons.checked),names(all.neurons.flow))
 if(length(missed)){
   missed.flow = hemibrainr::add_Label(all.neurons.checked[as.character(missed)], Label = 3, internal.assignments = TRUE)
-  all.neurons.flow[as.character(missed)] = missed.flow
+  all.neurons.flow = all.neurons.flow[setdiff(names(all.neurons.flow),names(missed.flow))]
+  all.neurons.flow = c(all.neurons.flow,missed.flow)
 }
 message("Flow centrality calculated for ", length(all.neurons.flow), " neurons")
 sensories = unique(c(rn.ids,orn.ids,hrn.ids))
@@ -54,18 +55,18 @@ all.neurons.points = hemibrain_splitpoints(all.neurons.flow)
 write.csv(all.neurons.points, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_splitpoints_",identifier,".csv"), row.names=FALSE)
 message("Split points: ", nrow(all.neurons.points))
 
-# Save compressed verssio
+# Save compressed version
 all.neurons.points.comp = subset(all.neurons.points, !grepl("null",all.neurons.points$point))
 write.csv(all.neurons.points.comp, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_splitpoints_",identifier,"_compressed.csv"), row.names=FALSE)
 message("Compressed split points: ", nrow(all.neurons.points.comp))
 
 # Get Synapses
-all.neurons.flow.syns = hemibrain_extract_synapses(all.neurons.flow)
+all.neurons.flow.syns = hemibrain_extract_synapses(all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
 write.csv(all.neurons.flow.syns, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_synapses_",identifier,".csv"), row.names=FALSE)
 message("Saved synapses: ", nrow(all.neurons.flow.syns))
 
 # Get connections
-all.neurons.flow.conns = hemibrain_extract_connections(all.neurons.flow)
+all.neurons.flow.conns = hemibrain_extract_connections(all.neurons.flow, .parallel = TRUE, OmitFailures = TRUE)
 write.csv(all.neurons.flow.conns, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_connections_",identifier,".csv"), row.names=FALSE)
 message("Saved connections: ", nrow(all.neurons.flow.conns))
 
@@ -77,11 +78,10 @@ message("Saved connections: ", nrow(all.neurons.flow.elist))
 # Calculate and save interesting metrics on this split
 all.neurons.flow = all.neurons.flow*(8/1000) # convert to microns
 mets = hemibrain_compartment_metrics(all.neurons.flow, OmitFailures = TRUE, .parallel = TRUE, delta = 5, resample = NULL)
+mets = t(apply(mets, 1, unlist))
 write.csv(mets, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_metrics_",identifier,".csv"), row.names=FALSE)
 message("Metrics calculated for neurons: ", nrow(mets))
 
 # Make a dotprops object
 all.neurons.flow.dps = nat::dotprops(all.neurons.flow, Labels = TRUE, resample = 1, OmitFailures = TRUE, .parallel = TRUE)
 save(all.neurons.flow.dps, file = paste0("/net/flystore3/jdata/jdata5/JPeople/Alex/FIBSEM/data/neurons/fibsem/hemibrain_all_neurons_flow_",identifier,"_dps.rda"))
-
-
