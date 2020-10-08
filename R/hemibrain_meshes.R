d82681cf456b48170bda57702cc12269936b7b34
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-10-08 18:15:14 +0100

    Update hemibrain_meshes.R

--- R/hemibrain_meshes.R
+++ R/hemibrain_meshes.R
@@ -93,3 +93,9 @@ hemibrain_read_neuronmesh_single <-function(x){
                                         cloudvolume.url='precomputed://gs://neuroglancer-janelia-flyem-hemibrain/segmentation_52a13')
 
 }
+
+# read hemibrain meshes
+# hemibrain_read_neuronmesh_single <-function(x){
+#   neuron_mesh = read_cloudvolume_meshes(segments = x, cloudvolume.url='precomputed://gs://neuroglancer-janelia-flyem-hemibrain/segmentation_52a13')
+#   neuron_mesh
+# }
