3b353faddf9d143850f36164e17ce76da6b5ce18
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-10-09 18:34:47 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -12,6 +12,13 @@
                                    options()$flywire_flagged_gsheet))
   }else{
     options(Gdrive_hemibrain_data = "/Volumes/GoogleDrive/Shared\ drives/hemibrainr/")
+    # Set google sheets of interest
+    options(hemibrainr_matching_gsheet = "1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
+    options(flywire_lineages_gsheet = "1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
+    options(flywire_flagged_gsheet = "1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
+    options(hemibrainr_gsheets = c(options()$hemibrainr_matching_gsheet,
+                                   options()$flywire_lineages_gsheet,
+                                   options()$flywire_flagged_gsheet))
   }
 }
 
