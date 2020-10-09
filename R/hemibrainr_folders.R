e702a023887131d7721e8d4464ed08b422c986f8
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-10-09 18:34:44 +0100

    Update hemibrainr_folders.R

--- R/hemibrainr_folders.R
+++ R/hemibrainr_folders.R
@@ -1,7 +1,7 @@
 
 # hidden
 hemibrainr_folder_structure <- function(start = options()$hemibrain_data){
-  brains = c("FAFB14", "FlyWire", "JRCFIB2018F","JRCFIB2018F","FAFB","JFRC2","JRC2018F","FCWB", "JFRC2013")
+  brains = c("FAFB14", "FlyWire", "JRCFIB2018F","JRCFIB2018Fraw","JFRC2","JRC2018F","FCWB","JFRC2013")
   hemibrain.neurons = paste0(start,"hemibrain_neurons","/",brains,"/data/")
   flywire.neurons = paste0(start,"flywire_neurons","/",brains,"/data/")
   for(f in c(hemibrain.neurons,flywire.neurons)){
