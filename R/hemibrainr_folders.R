eab0afaca2190e8b84cbf3bd2f3318deb102cea7
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-10-13 21:05:30 +0100

    Update hemibrainr_folders.R

--- R/hemibrainr_folders.R
+++ R/hemibrainr_folders.R
@@ -1,13 +1,16 @@
 
 # hidden
-hemibrainr_folder_structure <- function(start = options()$hemibrain_data){
+hemibrainr_folder_structure <- function(start = options()$hemibrain_data,
+                                        team.drive = "hemibrain"){
+
+  # Make local folders
   brains = c("FAFB14", "FlyWire", "JRCFIB2018F","JRCFIB2018Fraw","JFRC2","JRC2018F","FCWB","JFRC2013")
   hemibrain.neurons = paste0(start,"hemibrain_neurons","/",brains,"/data/")
   flywire.neurons = paste0(start,"flywire_neurons","/",brains,"/data/")
   for(f in c(hemibrain.neurons,flywire.neurons)){
     dir.create(f, recursive = TRUE)
   }
-  dir.create("hemibrain_nblast", recursive = TRUE)
+  dir.create(paste0(start,"hemibrain_nblast", recursive = TRUE))
 }
 
 # hidden
