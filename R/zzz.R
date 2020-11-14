3dc99e9ae1b4e75c403d10bd931fce1bbe448e0a
Gregory Jefferis <jefferis@gmail.com>
2020-11-14 16:34:51 +0000

    Check more shared drives (hemibrain, option)
    * should check to see if the option is actually set!
    * also check hemibrain shared drive as well
    * remove duplicated code

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,19 @@
 # set options
 .onAttach <- function(libname, pkgname){
   options(hemibrain_data = paste0(getwd(),"/data-raw/hemibrainr_data/"))
-  if(file.exists("/Volumes/GoogleDrive/Shared\ drives/hemibrainr/")){
-    options(Gdrive_hemibrain_data = "/Volumes/GoogleDrive/Shared\ drives/hemibrainr/")
-    # Set Google sheets of interest
-    options(hemibrainr_matching_gsheet = "1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
-    options(flywire_lineages_gsheet = "1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
-    options(flywire_flagged_gsheet = "1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
-    options(hemibrainr_gsheets = c(options()$hemibrainr_matching_gsheet,
-                                   options()$flywire_lineages_gsheet,
-                                   options()$flywire_flagged_gsheet))
+
+  paths=c(
+    getOption("Gdrive_hemibrain_data"),
+    file.path("/Volumes/GoogleDrive/Shared\ drives",
+    c("hemibrain", "hemibrainr")))
+  pathsok=file.exists(paths)
+
+  if(any(pathsok)){
+    options(Gdrive_hemibrain_data = which(pathsok)[1])
   }else{
+    # fallback
+    options(Gdrive_hemibrain_data = paste0(getwd(),"/data-raw/hemibrainr_data/"))
+
     packageStartupMessage("
   ####################### remote data ########################
   To use data grabbing functions in hemibrainr please point
@@ -27,14 +30,14 @@
   or ?hemibrainr_set_drive
   ####################### remote data ########################
   ")
-    # Set Google sheets of interest
-    options(Gdrive_hemibrain_data = paste0(getwd(),"/data-raw/hemibrainr_data/"))
-    options(hemibrainr_matching_gsheet = "1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
-    options(flywire_lineages_gsheet = "1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
-    options(flywire_flagged_gsheet = "1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
-    options(hemibrainr_gsheets = c(options()$hemibrainr_matching_gsheet,
-                                   options()$flywire_lineages_gsheet,
-                                   options()$flywire_flagged_gsheet))
   }
+
+  # Set Google sheets of interest
+  options(hemibrainr_matching_gsheet = "1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
+  options(flywire_lineages_gsheet = "1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
+  options(flywire_flagged_gsheet = "1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
+  options(hemibrainr_gsheets = c(options()$hemibrainr_matching_gsheet,
+                                 options()$flywire_lineages_gsheet,
+                                 options()$flywire_flagged_gsheet))
 }
 
