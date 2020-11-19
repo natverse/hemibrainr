ffeaa2be040f759e2e68ee1d28fc459f288db06d
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-11-19 16:57:29 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   options(hemibrain_data = paste0(getwd(),"/data-raw/hemibrainr_data/"))
 
   paths=c(
-    getOption("Gdrive_hemibrain_data"),
+    nullToNA(getOption("Gdrive_hemibrain_data")),
     file.path("/Volumes/GoogleDrive/Shared\ drives",
     c("hemibrainr", "hemibrain")))
   pathsok=file.exists(paths)
@@ -33,9 +33,9 @@
   }
 
   # Set Google sheets of interest
-  hemibrainr_matching_gsheet = ifelse(is.null(getOption("hemibrainr_matching_gsheet")),getOption("hemibrainr_matching_gsheet"),"1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
-  flywire_lineages_gsheet = ifelse(is.null(getOption("flywire_lineages_gsheet")),getOption("flywire_lineages_gsheet"),"1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
-  flywire_flagged_gsheet = ifelse(is.null(getOption("flywire_flagged_gsheet")),getOption("flywire_flagged_gsheet"),"1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
+  hemibrainr_matching_gsheet = ifelse(!is.null(getOption("hemibrainr_matching_gsheet")),getOption("hemibrainr_matching_gsheet"),"1OSlDtnR3B1LiB5cwI5x5Ql6LkZd8JOS5bBr-HTi0pOw")
+  flywire_lineages_gsheet = ifelse(!is.null(getOption("flywire_lineages_gsheet")),getOption("flywire_lineages_gsheet"),"1spGSuhUX6Hhn-8HH0U_ArIWUuPpMBFNjIjeSSh_MFVY")
+  flywire_flagged_gsheet = ifelse(!is.null(getOption("flywire_flagged_gsheet")),getOption("flywire_flagged_gsheet"),"1rzG1MuZYacM-vbW7100aK8HeA-BY6dWAVXQ7TB6E2cQ")
   options(hemibrainr_matching_gsheet = hemibrainr_matching_gsheet)
   options(flywire_lineages_gsheet = flywire_lineages_gsheet)
   options(flywire_flagged_gsheet = flywire_flagged_gsheet)
