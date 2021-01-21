40c9e209baa76e47545dad5afe9b4489db3b1cd6
alexanderbates <alexander.shakeel.bates@gmail.com>
2021-01-21 19:47:47 +0000

    Set of functions to read flywire tracing sheets
    * Read tracing sheets
    * Go to the right tab in a tracing sheet
    * See if your flywire neuron is already in a tracing sheet

---
+++ R/flywire_tracing.R
@@ -0,0 +1,183 @@
+# lineage google sheets
+
+#' Retrieve or go to a specific google sheet describing flywire neurons.
+#'
+#' @description These functions can help you
+#' either go to or read the right tab (\code{flywire_tracing_sheet}) on a large google sheet, or read the whole sheet and all of its tabs ((\code{flywire_tracing_sheets}),
+#' note, memoised for 30 minutes).
+#' You can also see if a flywire neuron of interest in in a google sheet (\code{flywire_in()}),
+#' and see if a flywire neuron of interest is among the data stored on the hemibrainr drive, i.e. (\code{flywire_in(\link{flywire_meta()})}).
+#'
+#' @param regex character vector. the google sheet(s) to update. Each entry is a unique google sheet ID. You can find these in a sheet's URL.
+#' If \code{NULL} then defaults to \code{option('hemibrainr_gsheets')}.
+#' @param open logial, if \code{TRUE} the relevant google sheet tab is opened in your browser. Else, a \code{data.frame} of the tab is returned.
+#' @param selected_sheet character vector indicating the a flywire tracing google sheet. This defaults to the master 'lineage' tracing sheet used by the Drosphila Connectomics Group to
+#' store annotations on flywire neurons in different developmental lineages. Note: You may not have access.
+#' @param query a vector of data indicating flywire neurons. These can be a vector of flywire XYZ positions (readable by \code{nat::xyzmatrix}),
+#' flywire root IDs or flywire supervoxel IDs. These are used to get the most up to date root IDs from FlyWire, which are then matched to the
+#' results from \code{flywire_tracing_sheets}.
+#' @param query.type  wtherh the qurey is a vector of xyz positions, flywire supervoxel IDs or flywire IDs ("flywire.xyz"/"flywire.svid"/"flywire.id").
+#' @param flywire_tracing_sheets a \code{data.frame} that is representative of the FAFB hemilineage survey google sheet, as produced by \code{flywire_tracing_sheets}.
+#' @param work_sheets a character vector of work sheet, i.e. tab, names for the googlesheet. If given, this function will only update those tabs.
+#' @param cloudvolume.url URL for CloudVolume to fetch segmentation image data. The default value of NULL choose
+#' @param ... methods passed to \code{googlesheets4} functions.
+#'.
+#' @details flywire tracing google sheets should have the columns: flywire.xyz (a single cardinal position for each neuron in raw FlyWire voxel space),
+#'flywire.svid (the supervoxel ID for this position) and flywire.id (the flywire root ID for the neuron that can be found at this position). Additional, informative columns
+#'are encouraged. A sheet in this format can be updated using \code{\link{flywire_ids_update}}.
+#'This updates the 'unstable' flywire.id column based on the stable flywire.xyz column, and also provides
+#'stable flywire.svid column if it is empty.
+#'
+#' @return a \code{data.frame} with columns from the right tab, chosen using \code{regex}, from the given google sheet(s) \code{selected_sheet},
+#' specified using the argument \code{chosen.columns}.
+#'
+#' @examples
+#' \donttest{
+#' \dontrun{
+#'
+#' # Quick look at what we have on the google sheet
+#' sheetlist=googlesheets4::sheet_properties(
+#' "1QyuHFdqz705OSxXNynD9moIsLvZGjjBjylx5sGZP2Yg")
+#' View(sheetlist)
+#'
+#' # Go to specific tab of lineage google sheet
+#' flywire_tracing_sheet("AOTUv1_v", open = TRUE)
+#' flywire_tracing_sheet("CREa1_v.*_r", open = TRUE)
+#'
+#' # Get this tab as a data frame
+#' flywire.lineage.sheet = flywire_tracing_sheet("CREa1_v.*_l")
+#'
+#' # Get the whole sheet as a data frame
+#' flywire.lineage.sheet = flywire_tracing_sheets()
+#' ## Note, the memoisation lasts for 30 minutes.
+#' ## I.e. this takes a long time to run but is saved for
+#' ## 30 mins on yuor computer. So if the function is called again
+#' ## with the same arguments, the sheets are not read again.
+#'
+#' # See if we have a certain flywire neuron in our sheets
+#' in.lin = flywire_in(query = "111437.5,21121.25,2661",
+#' query.type = "flywire.xyz", Verbose = TRUE,
+#' fw.meta = flywire_tracing_sheet())
+#'
+#' # And in the meta data for neurons on the google drive
+#' in.meta = flywire_in(query = "111437.5,21121.25,2661",
+#' query.type = "flywire.xyz", Verbose = TRUE)
+#'
+#' }}
+#' @seealso \code{\link{flywire_ids}},
+#'   \code{\link{flywire_meta}},
+#'   \code{\link{flywire_neurons}}
+#' @name flywire_tracing_sheet
+#' @export
+flywire_tracing_sheet <- function(regex,
+                                  open=FALSE,
+                                  selected_sheet = options()$flywire_lineages_gsheet,
+                                  ...) {
+  if(open){
+    open = interactive()
+  }
+  u= sprintf("https://docs.google.com/spreadsheets/d/%s/edit", selected_sheet)
+  props=sheet_properties.memo(u)
+  m=grepl(regex, props$name, ...)
+  sel=props[m,]
+  if(nrow(sel)>1) {
+    warning("Multiple matches. Keeping first: ", paste(sel$name, collapse = ','))
+    sel=sel[1,,drop=F]
+  } else if(nrow(sel)==0) {
+    stop("No matches!")
+  }
+  uu=paste0(u, "#gid=", sel$id)
+  if(open){
+    browseURL(uu)
+  }else{
+    gsheet_manipulation(FUN = googlesheets4::read_sheet,
+                                     wait = 20,
+                                     ss = selected_sheet,
+                                     guess_max = 3000,
+                                     sheet = sel$name,
+                                     return = TRUE,
+                        ...)
+  }
+}
+
+#' @export
+#' @rdname flywire_tracing_sheet
+flywire_tracing_sheets <- memoise::memoise(function(work_sheets = NULL,
+                                                    selected_sheet = options()$flywire_lineages_gsheet){
+  flywire_tracing_sheets.now(work_sheets = work_sheets, selected_sheet=selected_sheet)
+},  ~memoise::timeout(30*60))
+
+# hidden
+flywire_tracing_sheets.now <- function(work_sheets = NULL,
+                                        selected_sheet = options()$flywire_lineages_gsheet){
+  wss = hemibrainr:::gsheet_manipulation(FUN = googlesheets4::sheet_names,
+                                          ss = selected_sheet,
+                                          return = TRUE)
+  gs.lineages  = data.frame(stringsAsFactors = FALSE)
+  if(!is.null(work_sheets)){
+    tabs = intersect(work_sheets,wss)
+  }else{
+    tabs = wss
+  }
+  for(tab in unique(tabs)){
+    gs.lin = hemibrainr:::gsheet_manipulation(FUN = googlesheets4::read_sheet,
+                                              wait = 20,
+                                              ss = selected_sheet,
+                                              sheet = tab,
+                                              guess_max = 3000,
+                                              return = TRUE)
+    gs.lin$ws = tab
+    gs.lineages = plyr::rbind.fill(gs.lineages, gs.lin)
+  }
+  gs.lineages
+}
+
+#' @export
+#' @rdname flywire_tracing_sheet
+flywire_in <- function(query,
+                             query.type = c("flywire.id","flywire.xyz","flywire.svid"),
+                             work_sheets = NULL,
+                             fw.meta = flywire_meta(),
+                             cloudvolume.url = NULL,
+                             Verbose = TRUE){
+  # Match arguments
+  query.type = match.arg(query.type)
+  if(Verbose){
+    message(sprintf("Looking for %s queries of type %s",length(query),query.type))
+  }
+  # Get SVIDS from sheets
+  sheet.svids = if(is.null(fw.meta$flywire.svid)){
+    fafbseg::flywire_xyz2id(fw.meta$flywire.xyz, rawcoords = TRUE, root=FALSE)
+  }else{
+    fw.meta$flywire.svid
+  }
+  # Get query as a SVID
+  if(query.type=="flywire.xyz"){
+    flywire.ids = fafbseg::flywire_xyz2id(nat::xyzmatrix(query), rawcoords = TRUE, cloudvolume.url=cloudvolume.url)
+  }else if(query.type=="flywire.svid"){
+    flywire.ids = fafbseg::flywire_rootid(query, cloudvolume.url=cloudvolume.url)
+  }
+  flywire.svids = lapply(flywire.ids, fafbseg::flywire_leaves, cloudvolume.url=cloudvolume.url)
+  names(flywire.svids) = query
+  df = data.frame(stringsAsFactors = FALSE)
+  for(fi in query){
+    svids = unlist(flywire.svids[fi])
+    idx = which(sheet.svids%in%svids)
+    if(!length(idx)||is.na(idx)){
+      next
+    }else{
+      df = plyr::rbind.fill(df, cbind(query=query, fw.meta[idx,]))
+    }
+  }
+  if(Verbose){
+    message(sprintf("%s of %s queries found:",sum(query%in%df$query),length(query)))
+    key.cols = c("query", "flywire.xyz", "flywire.svid","flywire.id", "status", "cell.type", "ItoLee_Hemilineage",
+                 "side", "hemibrain.match", "hemibrain.match.quality", "tab", "ws")
+    knitr::kable(df[,intersect(key.cols,colnames(df))])
+  }
+  df
+}
+
+# hidden, caches result for 5min in current session
+sheet_properties.memo <- memoise::memoise(googlesheets4::sheet_properties, ~memoise::timeout(5*60))
+
