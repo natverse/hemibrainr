fbd5af9403e1dd41df3511ffccd2a6eccf68b7a1
alexanderbates <alexander.shakeel.bates@gmail.com>
2021-03-10 21:44:26 -0500

    roxygenise

--- R/neuron_morphology.R
+++ R/neuron_morphology.R
@@ -6,6 +6,7 @@
 #' @param x a \code{neuron} or \code{neuronlist} object.
 #' @param Label defaults to 1, so distances to soma are chiosen. If 7, then distance from primary neurite's branchpoint.
 #' @param graph.distance logical, whether to use geodesic distance in terms of node number (\code{TRUE}) or real unit distance.
+#' @param ... methods passed to \code{nat::nlapply}
 #' @name geodesic_distance
 geodesic_distance <- function(x, graph.distance=TRUE, Label = 1, ...) UseMethod("geodesic_distance")
 
