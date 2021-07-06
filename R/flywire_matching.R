36a43cbbd17ffdca975d10b2a7eaede83ad2e477
alexanderbates <alexander.shakeel.bates@gmail.com>
2021-07-05 20:07:11 -0400

    Update flywire_matching.R

--- R/flywire_matching.R
+++ R/flywire_matching.R
@@ -363,7 +363,7 @@ neuron_match_scanner <- function(brain,
         rgl::bg3d("white")
         plot3d(brain, alpha = 0.1, col ="grey")
         query.n = get_match_neuron(query = query, n = n, query.repository = query.repository, skip.if.absent = TRUE)
-        if(!is.null(query.n)){plot3d(query.n, lwd = 3, soma = soma.size, col = "#1BB6AF")}
+        if(!is.null(query.n)&&!length(query.n)){plot3d(query.n, lwd = 3, soma = soma.size, col = "#1BB6AF")}
         progress = readline(prompt = "This neuron will be skipped. Press any key to continue ")
         next
       }
