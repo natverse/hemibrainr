5ffcdf06533c3e53fb7f7b3f84f575fe73f1ded9
alexanderbates <alexander.shakeel.bates@gmail.com>
2022-12-01 00:48:29 -0500

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -469,7 +469,7 @@ skeletor_batch <- function(obj, swc, numCores = 1, multiplier = 10, max.file.siz
     neuron.ids = batches[[batch]]
     skels = suppressMessages(fafbseg::skeletor(neuron.ids, save.obj = NULL, mesh3d = FALSE))
     skels[,"id"] = names(skels) = basename(gsub("\\.obj","",names(skels)))
-    nat::write.neurons(skels, dir=swc, format='swc', Force = FALSE)
+    nat::write.neurons(skels, dir=swc, format='swc', Force = TRUE)
     NULL
   }
 
